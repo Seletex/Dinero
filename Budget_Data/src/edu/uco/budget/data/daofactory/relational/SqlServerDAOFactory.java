6b34bfe4424074d5cc8528e24b0c0dee6381ad9f
Seletex <sarr1998@hotmail.com>
2022-10-14 18:35:41 -0500

    fix

--- Budget_Data/src/edu/uco/budget/data/daofactory/relational/SqlServerDAOFactory.java
+++ Budget_Data/src/edu/uco/budget/data/daofactory/relational/SqlServerDAOFactory.java
@@ -1,5 +1,58 @@
 package edu.uco.budget.data.daofactory.relational;
 
-public class SqlServerDAOFactory {
+import edu.uco.budget.data.dao.BudgetDAO;
+import edu.uco.budget.data.dao.PersonDAO;
+import edu.uco.budget.data.dao.YearDAO;
+import edu.uco.budget.data.daofactory.DAOFactory;
+
+public final class SqlServerDAOFactory extends DAOFactory {
+
+	@Override
+	protected void openConexion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void confirmTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BudgetDAO getBudgetDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PersonDAO getPersonDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public YearDAO getYearDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
